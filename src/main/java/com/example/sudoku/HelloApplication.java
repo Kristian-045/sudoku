a57533b322272555c9eb806b767f04f50dea2fe5
Kristian-045 <kaleta.kristian@gmail.com>
2022-04-08 23:00:30 +0200

    design of first sudoku grid, add css to main, add few comments for understanding

--- src/main/java/com/example/sudoku/HelloApplication.java
+++ src/main/java/com/example/sudoku/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.sudoku;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 576);
+
+        stage.setTitle("Sudoku solver");
+//        ked budeme mat nejaku ikonku tak ju tam mozme dat
+//        stage.getIcons().add(new Image("file:src/main/resources/app/mediaplayer/7215-youtube-playbutton.png"));
+            scene.getStylesheets().add(getClass().getResource("style.css").toString());
+
         stage.setScene(scene);
         stage.show();
     }
