77476cdfde34b631e8d41b5be42bfef85b1f29a7
Kristian-045 <kaleta.kristian@gmail.com>
2022-04-21 13:33:23 +0200

    ano

--- src/main/java/com/example/sudoku/HelloController.java
+++ src/main/java/com/example/sudoku/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.sudoku;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Arrays;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void highlight(MouseEvent event){
+        String id =  event.getSource().toString().split("id=")[1].substring(0,6);
+
+
     }
 }
